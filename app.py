4e794b06bdd7a28cf64e8370422264da0bdbcb84
Pol Burkardt Freire <pol.burkardt@estudiantat.upc.edu>
2022-03-15 13:07:08 +0100

    Prepare environment

--- app.py
+++ app.py
@@ -14,24 +14,39 @@ app = Flask(__name__)
 def helloWorld():
   return "PES Econnect Root!"
 
-@app.route("/account")
+@app.route("/account", methods = ['POST'])
 def registerUser():
   # TODO: Use POST request instead of test data
+
+  if request.method == 'POST':
+    # Create account
+    username = request.form['username']
+    username = request.form['email']
+    password = request.form['password']
+
+    username = "TestUser" + str(randint(1, 10000))
+    password = "TestPass" + str(randint(1, 10000))
+
+    try:
+      User.register(username, password)
+      
+      # User created successfully
+      return 0; 
+
+    except UsernameExistsException as ue:
+      print(ue.message) # Debug
+      
+      # Failed to create user because username already exists
+      return ERR_USERNAME_EXISTS;
+
+@app.route("/admin/login")
+def adminUser():
+  n = request.args.get('name')
+  p = request.args.get('password')
   
-  username = "TestUser" + str(randint(1, 10000))
-  password = "TestPass" + str(randint(1, 10000))
-
-  try:
-    User.register(username, password)
-    
-    # User created successfully
-    return 0; 
-
-  except UsernameExistsException as ue:
-    print(ue.message) # Debug
-    
-    # Failed to create user because username already exists
-    return ERR_USERNAME_EXISTS;
+  return {
+    'atok': getAdminToken(n, p)
+  }
 
 
 if __name__ == "__main__":
