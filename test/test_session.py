f4b57000e05595cd490a4af1d4a91934502a836f
silvia.andreu <silvia.andreu@estudiantat.upc.edu>
2022-04-08 19:08:45 +0200

    test session finished

--- test/test_session.py
+++ test/test_session.py
@@ -2,58 +2,60 @@ from app import app
 import json
 import hashlib
 
+import data.DBUtils as db
+
+global token
+
 def test_homePage():
     response = app.test_client().get("/")
     assert response.status_code == 200
     assert response.data == b"PES Econnect Root!"
 
 def test_isAdmin():
-    response = app.test_client().get("/account/isadmin", data=dict(email="admin@econnect.com", password="admin"))
+    response = app.test_client().get("/account/isadmin?token=0cdde536-b597-11ec-8ddb-7aa110a1837a")
     assert response.status_code == 200
+    assert response.data == b'{"result":"true"}\n'
 
 def test_isAdminNotFound():
-    response = app.test_client().get("/account/isadmin", data=dict(email="admin@econnect.cat", pasword="admin"))
+    response = app.test_client().get("/account/isadmin?email=admin@email.com&password=adminn")
     assert response.data == b'{"error":"ERROR_INVALID_TOKEN"}\n'
 
 def test_isAdminIncorrectPassword():
-    response = app.test_client().get("/account/isadmin", data=dict(email="admin@econnect.com", password="hola"))
+    response = app.test_client().get("/account/isadmin?email=admin@econnectcom&password=adminnn")
     assert response.data == b'{"error":"ERROR_INVALID_TOKEN"}\n'
 
 def test_signUp():
-    response = app.test_client().post('/account', data=dict(email="test@test.com", username="test", password="pwdtest"))
+    # crea un usuario con los siguientes datos:
+    # username: signUpTest, password: signUpPwd, email: sign@up.test
+    response = app.test_client().post("/account?username=signUpTest&password=signUpPwd&email=sign@up.test")
+    status = response.json['status']
     assert response.status_code == 200
-
-
-'''
-# LOGIN
-
-def test_accountLogIn():
-    response = app.test_client().get("/account/login", data=dict(email="gmail", password="1234"))
-    assert response.status_code == 200
-    assert response.data is not null
-
-def test_accountLogInUserNotFound():
-
-def test_accountLogInIncorrectPassword():
-
-def test_accountLoginFailedToOpenSession():
-
-# SIGNUP
-
-def test_signUp():
+    assert status == 'success'
 
 def test_signUpEmailExists():
+    response = app.test_client().post("/account?username=signUpTest&password=signUpPwd&email=sign@up.test")
+    assert response.data == b'{"error":"ERROR_USER_EMAIL_EXISTS"}\n'
 
 def test_signUpUsernameExists():
+    response = app.test_client().post("/account?username=signUpTest&password=signUpPwd&email=sign@up.test2")
+    assert response.data == b'{"error":"ERROR_USER_USERNAME_EXISTS"}\n'
 
-# LOGOUT
-
-def test_logOut():
+def test_accountLogIn():
+    response = app.test_client().get("/account/login?email=sign@up.test&password=signUpPwd")
+    token = response.data
     assert response.status_code == 200
-    
-def test_logOut():
+    assert token != None
 
-def test_logOutInvalidToken():
+def test_accountLogInUserNotFound():
+    response = app.test_client().get("/account/login?email=newSign@up.test&password=signUpPwd")
+    assert response.data == b'{"error": "ERROR_USER_NOT_FOUND"}'
 
-'''
+def test_accountLogInIncorrectPassword():
+    response = app.test_client().get("/account/login?email=sign@up.test&password=signUpPwdWrong")
+    assert response.data == b'{"error": "ERROR_USER_INCORRECT_PASSWORD"}'
+
+def test_cleanDB():
+    # clean database
+    db.delete("DELETE FROM users where email in ('sign@up.test')")
+
 
