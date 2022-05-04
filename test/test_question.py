0b9311d2705011f4d6735adadb7d03feb5995690
pauantonio <pauantoniosoler@gmail.com>
2022-05-04 23:41:39 +0200

    questions tests

--- test/test_question.py
+++ test/test_question.py
@@ -1,5 +1,8 @@
 from app import app
 
+import data.DBUtils as db
+
+
 '''
 
 # GET QUESTIONS
@@ -12,3 +15,24 @@ def test_answerProductQuestion():
 
 def test_answerCompanyQuestion():
 '''
+
+
+def test_initDB():
+    db.insert("INSERT INTO sessiontoken VALUES ('93003eec-b589-11ec-a4e2-00155d3ce0fb',1)")
+    db.insert("INSERT INTO question VALUES (1, 1, 'test')")
+
+
+def test_modifyQuestion():
+    response = app.test_client().put("questions/1?token=93003eec-b589-11ec-a4e2-00155d3ce0fb&newQuestion=haha")
+    assert response.status_code == 200
+    assert response.data == b'{"status":"success"}\n'
+
+
+def test_deleteQuestion():
+    response = app.test_client().delete("questions/1?token=93003eec-b589-11ec-a4e2-00155d3ce0fb")
+    assert response.status_code == 200
+    assert response.data == b'{"status":"success"}\n'
+
+
+def test_cleanDB():
+    db.delete("DELETE FROM sessiontoken where token = '93003eec-b589-11ec-a4e2-00155d3ce0fb'")
